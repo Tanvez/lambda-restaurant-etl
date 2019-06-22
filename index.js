b24fabf556c806d41e076cb22fb0a49bba3ac5cc
Vesna Tan <tanvezi@gmail.com>
2019-06-22 16:33:09 -0700

    Add insertion query to RDS for test row

--- index.js
+++ index.js
@@ -7,8 +7,47 @@ const { promiseWrapper, csvReadUrl } = require('./utils');
 exports.handler = async (event, context, callback) => {
   const { URL, DB_COLS } = process.env;
   const parsedColArray = JSON.parse(DB_COLS);
-  // console.log(parsedColArray);
-  // const result = await csvReadUrl(URL);
-  // const data = result[0];
-  // console.log(result[0]);
+  const result = await csvReadUrl(URL);
+  const data = result[0];
+
+  const client = new Client({
+    user: process.env.DB_USERNAME,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+  const connection = await promiseWrapper(client.connect());
+  if (!connection.ok) {
+    console.error(new Error(`Error -->:${connection.error}`));
+    return callback({
+      statusCode: 500,
+      error: `Error -->:${connection.error}`,
+    });
+  }
+
+  // console.log({ connection });
+
+  const cols = parsedColArray.join(', ');
+  let placeholders = [];
+  parsedColArray.forEach((e, idx) => placeholders.push(`$${idx + 1}`));
+  placeholders = `${placeholders.join(
+    ', '
+  )}, $11, $12, $13, $14, $15, $16, $17, $18, NOW(), NOW()`;
+  console.log({ placeholders, cols });
+
+  const query = `INSERT INTO public."restaurants" (${cols},"cuisineDescription","inspectionDate","violationCode","violationDescription","criticalFlag","gradeDate","recordDate","inspectionType", "createdAt", "updatedAt") VALUES(${placeholders})`;
+  const dbRes = await promiseWrapper(
+    client
+      .query(query, data)
+      .then(res => {
+        client.end();
+        return res;
+      })
+      .catch(e => {
+        console.error(e);
+        client.end();
+      })
+  );
+  return callback(null, { dbRes, status: 200 });
 };
